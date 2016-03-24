fd955f87cc0657c28848d906b6ba2c7dcfe34544
Andrew Bartuska <andrew.bartuska@duke.edu>
2016-03-24 16:36:16 -0400

    Created tests for load_data, fix_tables, split_targets

--- test/test_fear_study.py
+++ test/test_fear_study.py
@@ -1,8 +1,20 @@
 from fear_study import *
 from nose.tools import *
 
+# Questions for John- Using outputs from other functions in tests
+
 def test_import_data():
-	
+	df, tobii_sync, task_data = import_data("test/test_data/tobii_test.csv", \
+						 	"test/test_data/test_sync_pulse_real.json", \
+							"test/test_data/test_faces_run.json")
+	data = []
+	if (df.empty == False and bool(tobii_sync) == True and bool(task_data) == True):
+		data.append(df)
+		data.append(tobii_sync)
+		data.append(task_data)
+	else:
+		raise TypeError("Some of the data is empty")
+	assert_equal(len(data), 3)
 
 def test_load_tobii_data():
 	df = load_tobii_data("test/test_data/tobii_test.csv")
@@ -17,3 +29,37 @@ def test_load_sync_pulses_data():
 def test_load_task_data():
 	task_data = load_task_data("test/test_data/test_faces_run.json")
 	assert_equal(type(task_data), dict)
+	
+def test_fix_table():
+	df, tobii_sync, task_data = import_data("test/test_data/tobii_test.csv", \
+						 	"test/test_data/test_sync_pulse_real.json", \
+							"test/test_data/test_faces_run.json")	
+	df = fix_tables(df, tobii_sync, task_data)
+	assert_equal(df.task_time.dtypes, float)
+	assert_equal(df.pupil_average.dtypes, float)
+	
+def test_split_target():
+	df, tobii_sync, task_data = import_data("test/test_data/tobii_test.csv", \
+						 	"test/test_data/test_sync_pulse_real.json", \
+							"test/test_data/test_faces_run.json")	
+	df = fix_tables(df, tobii_sync, task_data)
+	fear_trials, notfear_trials = split_target(df, task_data)
+	
+	assert_equal(type(fear_trials), np.ndarray)
+	assert_equal(type(fear_trials), np.ndarray)
+	
+	assert_equal(fear_trials.size == 0, False)
+	assert_equal(notfear_trials.size == 0, False)
+	
+	assert_equal(all(len(x) == len(fear_trials[0]) for x in fear_trials), True)
+	assert_equal(all(len(x) == len(notfear_trials[0]) for x in notfear_trials), True)
+	
+def test_baseline_norm_sub():
+	pass
+
+def test_baseline_norm_div():
+	pass
+
+def test_plot_comparisons():
+	pass
+
